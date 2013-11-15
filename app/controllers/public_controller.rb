959382ab8c5f904c25ddd0f081517aa4e342356d
Jeremy Sinitean <jeremy.sinitean@gmail.com>
2013-11-14 18:52:31 -0600

    Adding project

---
+++ app/controllers/public_controller.rb
@@ -0,0 +1,23 @@
+class PublicController < ActionController::Base
+  
+  layout 'public'
+  
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+  
+  def contact
+    
+  end
+  
+  def experience
+    
+  end
+  
+  def portfolio
+    
+  end
+  
+  
+  
+end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sinitean::Application.config.session_store :cookie_store, key: '_sinitean_session'
